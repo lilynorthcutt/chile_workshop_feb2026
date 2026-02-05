fce033dc71848d6765599b71c56611f7591f87d6
lilynorthcutt <northcuttlilya@gmail.com>
2026-02-04 22:56:22 -0700

    Set up script with package installs

---
+++ setup_script.R
@@ -0,0 +1,12 @@
+############################
+#### Pre-Workshop Setup ####
+############################
+
+### INSTALL PACKAGES ###
+packages <- c("agridat", "tidyr", "readxl", "dplyr", "magrittr", "purrr", 
+              "ggplot2", "stringr", "snakecase", "Hmisc", "lubridate", "plyr")
+# Install packages
+install.packages(packages, dependencies = TRUE,type = "source")
+
+
+library("dplyr")
